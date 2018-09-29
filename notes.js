7137d30afdefc0b7d9a13637508d0cdeb796face
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-09-29 12:18:29 +0530

    Restarting app by nodemon and code cleaning

--- notes.js
+++ notes.js
@@ -1,13 +1,8 @@
 console.log("Starting notes.js");
-// console.log(module);
 
 module.exports.age = 25; 
 
 module.exports.addNote = () => {
 	console.log("Add note");
 	return 'New note';
-};
-
-module.exports.add = (a,b) => {
-	return a+b;
 };
