ea54d190ee5283366a1810c877d881adc4b988c9
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-09-29 12:08:19 +0530

    Use third party module Lodash to remove duplicates from an array

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 
+var filteredArray = _.uniq(['a', "A", "a", 1, 'Mukesh', "Mukesh", "mukesh"]);
+console.log(filteredArray);
 
-var res = notes.addNote();
-console.log(res);
 
-var sum = notes.add(8,-3);
-console.log(sum);
+// console.log(_.isString("pp"));
+// console.log(_.isString(true));
+// console.log(_.isString(1));
+// console.log(_.isString('1'));
+
+
+// var res = notes.addNote();
+// console.log(res);
+
+// var sum = notes.add(8,-3);
+// console.log(sum);
 
 // const userInfo = os.userInfo();
 // fs.appendFile('greetings.txt', `Hello ${userInfo.username}. You are ${notes.age}`, function(err){
