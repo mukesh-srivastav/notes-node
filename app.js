5bdbb8f9a50322370457f77e62ec34312047c23c
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2018-09-29 12:44:52 +0530

    Getting user input commands from  terminal using process.argv

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+const command = process.argv[2];
+console.log('Command :', command); 
+console.log(process.argv);
+
+if (command === 'add') {
+	console.log('Adding a note');
+} else if (command === 'list') {
+	console.log('Listing all notes');
+} else if (command === 'read') {
+	console.log('Reading a note');
+} else if (command === 'remove') {
+	console.log('Removing a note');
+} else {
+	console.log('Unknown command');
+}
